da533f0bc99d1b88c7ba62c088a795805c331c64
omer358 <omermaki358@gmail.com>
2024-08-17 19:57:06 +0300

    1. create first cross-platform app

---
+++ shared/src/commonMain/kotlin/com/jetbrains/greeting/Greeting.kt
@@ -0,0 +1,12 @@
+package com.jetbrains.greeting
+
+import kotlin.random.Random
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        val firstWord = if(Random.nextBoolean()) "Hi" else "Hello"
+        return "$firstWord Guess what is this! > ${platform.name.reversed()}!"
+    }
+}
